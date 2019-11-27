d7d05371c8afbafb0e3a83f10d1c2a02e51d9de3
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-11-28 00:38:48 +0900

    chore: deploy to ecs

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"log"
 )
 
+func hello(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "nyaan")
+}
+
 func main(){
-	fmt.Println("nyaan")
+	http.HandleFunc("/", hello)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
