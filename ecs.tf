a9ad0cb4667d4ce45a39a46fff16599c1a06cffe
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-11-28 21:18:58 +0900

    chore: delete tf files

--- ecs.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_ecs_cluster" "cluster" {
-  name = "${var.service}-cluster"
-}
-
--- output.tf
+++
@@ -1,7 +0,0 @@
-output "alb" {
-  value = {
-    dns_name         = aws_lb.alb.dns_name
-    arn              = aws_lb.alb.arn
-    target_group_arn = aws_lb_target_group.group.arn
-  }
-}
