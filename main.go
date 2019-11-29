92d43a483e2b4637eb30cd5780bf287380de74fe
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-11-29 17:24:49 +0900

    feat: write data to influxDB

--- main.go
+++ main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"log"
+	"time"
+
+	_ "github.com/influxdata/influxdb1-client" 
+	client "github.com/influxdata/influxdb1-client/v2"
 )
 
 func hello(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "nyaan")
+	c, err := client.NewHTTPClient(client.HTTPConfig{Addr: "http://localhost:8086"})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.Close()
+
+	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database: "test",
+		Precision: "s",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tags := map[string]string{"tick": "tick"}
+	fields := map[string]interface{}{
+		"foo": "hoge",
+	}
+
+	pt, err := client.NewPoint("tick", tags, fields, time.Now())
+	if err != nil {
+		log.Fatal(err)
+	}
+	bp.AddPoint(pt)
+
+	c.Write(bp)
 }
 
 func main(){
+
 	http.HandleFunc("/", hello)
-	err := http.ListenAndServe(":80", nil)
+	log.Print("connect influxDB at localhost")
+
+	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
