c38d94d4a900680fe08e612dbe8024c62c9e97f9
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-11-30 19:12:49 +0900

    feat: eventAPI

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/kirisaki/slack-counter
 
 go 1.13
 
-require github.com/influxdata/influxdb1-client v0.0.0-20190809212627-fc22c7df067e
+require (
+	github.com/influxdata/influxdb1-client v0.0.0-20190809212627-fc22c7df067e
+	github.com/nlopes/slack v0.6.0
+)
--- main.go
+++ main.go
@@ -3,14 +3,58 @@ package main
 import (
 	"net/http"
 	"log"
-	"time"
+	//"time"
+	"bytes"
+	"encoding/json"
+	"os"
 
-	_ "github.com/influxdata/influxdb1-client" 
-	client "github.com/influxdata/influxdb1-client/v2"
+	//_ "github.com/influxdata/influxdb1-client" 
+	//client "github.com/influxdata/influxdb1-client/v2"
+
+	"github.com/nlopes/slack"
+	"github.com/nlopes/slack/slackevents"
+
 )
+func makeEventHandler(token string, verifyToken string)(func (http.ResponseWriter, *http.Request)) {
+	api := slack.New(token)
+	return func (w http.ResponseWriter, r *http.Request) {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(r.Body)
+		body := buf.String()
+		eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: verifyToken}))
+		if e != nil {
+			log.Print(e)
+			w.WriteHeader(http.StatusInternalServerError)
+		}
 
-func hello(w http.ResponseWriter, r *http.Request){
-	c, err := client.NewHTTPClient(client.HTTPConfig{Addr: "http://localhost:8086"})
+		if eventsAPIEvent.Type == slackevents.URLVerification {
+			var r *slackevents.ChallengeResponse
+			err := json.Unmarshal([]byte(body), &r)
+			if err != nil {
+				log.Print(err)
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+			w.Header().Set("Content-Type", "text")
+			w.Write([]byte(r.Challenge))
+		}
+		if eventsAPIEvent.Type == slackevents.CallbackEvent {
+			innerEvent := eventsAPIEvent.InnerEvent
+			switch ev := innerEvent.Data.(type) {
+			case *slackevents.AppMentionEvent:
+					log.Print("nyaan")
+			api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
+			default:
+				log.Print(ev)
+			}
+		}
+	}
+}
+
+
+/*
+func eventHandler(w http.ResponseWriter, r *http.Request){
+	
+	c, err := client.NewHTTPClient(client.HTTPConfig{Addr: "http://influxdb:8086"})
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -35,15 +79,26 @@ func hello(w http.ResponseWriter, r *http.Request){
 	}
 	bp.AddPoint(pt)
 
-	c.Write(bp)
+	log.Print("write measurement")
+	er := c.Write(bp)
+	if er != nil {
+		log.Print(er)
+	}
 }
+*/
 
 func main(){
+	token := os.Getenv("SLACK_TOKEN")
+	if token == "" {
+		log.Fatal("set SLACK_TOKEN")
+	}
+	verifyToken := os.Getenv("SLACK_VERIFY_TOKEN")
+	if token == "" {
+		log.Fatal("set SLACK_VERIY_TOKEN")
+	}
 
-	http.HandleFunc("/", hello)
-	log.Print("connect influxDB at localhost")
-
-	err := http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/event", makeEventHandler(token, verifyToken))
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
